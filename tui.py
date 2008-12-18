11289c522ae8b994498bf5b16b0f6cef992bcdb6
gatino <na@na.com>
2008-12-18 18:00:56 +0000

    UI fixes

--- tui.py
+++ tui.py
@@ -8,8 +8,23 @@ import logging
 
 class View(object):
 
+	def __init__(self, win, parent):
+		self.win = win
+		components = set()
+		if parent is not None:
+			self.parent.components.add(self)
+
+	def update(self):
+		for c in components:
+			c.update()
+		self.draw()
+	
 	def draw(self, canvas):
 		pass
+
+class TopLevelView(View):
+	def __init__(self, win):
+		View.__init__(self, win, None)
 
 class Controller(object):
 	pass
@@ -23,43 +38,53 @@ class SourceFileView(View):
 		
 		maxy, maxx = self.win.getmaxyx()
 		self.client_area = win.subwin(maxy-2, maxx-2, 1, 1)
-		self.top_line = 1
+		self.line_off = 0
 		self.src_file = None
-		self.src_line = None
+		self.src_line = None # 1-based
 		self.src_line_data = []
 
 		self.dirty = True
 		self.draw()
 
-		self.app.sess.onProcessedResponse.subscribe(self.onProcessedResponse)
 		self.app.sess.onBreakpointSet.subscribe(self.onBreakpointSet)
 		self.app.sess.onFrameChange.subscribe(self.onFrameChange)
 		
 	def __del__(self):
 		pass
-	
+
+	def _get_line_from_buffer(self, buf, lineno):
+		# just translate between 1-based line numbers in files
+		# and 0-based in buffers
+		return buf[lineno - 1]
+
 	def draw(self):
-		self.win.border()
-		maxy, maxx = self.client_area.getmaxyx()
-		self.client_area.clear()
-		startline = self.src_line if self.src_line is not None else 0
-		endline = startline + maxy
-		maxndigits = len(str(len(self.src_line_data)))
-		self.log.debug("SourceFileView : size (%d, %d) - startline %d - endline %d" % (maxy, maxx, startline, endline))
-		visible_lines = self.src_line_data[startline:endline]
-		for i in xrange(len(visible_lines)):
-			line = visible_lines[i]
-			lineno = i + startline
-			if line == self.src_line:
-				self.client_area.attron(curses.A_REVERSE)
-			else:
-				self.client_area.attroff(curses.A_REVERSE)
-			linedata = [' '] * maxx
-			linedata[:maxndigits] = str(lineno)
-			linedata[maxndigits+1:] = line
-			self.log.debug("SRC: %s" % line)
-			linedata = ''.join(linedata)
-			self.win.addnstr(i, 0, linedata, maxx)
+		if self.dirty: 
+			self.win.border()
+			maxy, maxx = self.client_area.getmaxyx()
+			self.client_area.clear()
+			startoff = self.line_off if self.line_off is not None else 0
+			startline = startoff + 1 # 'offsets' are 0-based, 'lines' are 1-based
+			endoff = startoff + maxy
+			endline = endoff + 1
+			maxndigits = len(str(len(self.src_line_data)))
+			self.log.debug("SourceFileView : size (%d, %d) - startoff %d - endoff %d" % (maxy, maxx, startoff, endoff))
+			visible_lines = self.src_line_data[startoff:endoff]
+			for i in xrange(len(visible_lines)):
+				lineno = i + startline 
+				line = visible_lines[i]
+				if lineno == self.src_line:
+					self.client_area.attron(curses.A_REVERSE)
+					self.client_area.attron(curses.A_BOLD)
+					self.log.debug("DRAWING WITH CURRENT LINE : %d" % lineno)
+				else:
+					self.client_area.attroff(curses.A_REVERSE)
+					self.client_area.attroff(curses.A_BOLD)
+				linedata = [' '] * maxx
+				linedata[:maxndigits] = str(lineno)
+				linedata[maxndigits+1:] = line
+				linedata = ''.join(linedata)
+				self.client_area.addnstr(i, 0, linedata, maxx)
+		self.dirty = False
 
 	def update_src_file(self, path):
 		if self.src_file != path:
@@ -68,24 +93,21 @@ class SourceFileView(View):
 			f = file(self.src_file, 'r')
 			self.src_line_data = f.readlines()
 			f.close()
-		self.draw()
-		self.win.refresh()
-
-	# Events 
-	def onProcessedResponse(self, session):
-		if self.dirty:
-			self.draw()
-			self.win.refresh()
-		self.dirty = False
 	
 	def onBreakpointSet(self, session, breakpoint_desc):
 		log.debug("EVENT : SourceFileView << onBreakPointSet")
-		self.dirty = True		
+		self.dirty = True
 
 	def onFrameChange(self, session, frame):
+		self.dirty = True
 		log.debug("EVENT : SourceFileView << onFrameChange")
-		self.update_src_file(frame.file)
-		self.draw()
+		if hasattr(frame, 'file'):
+			self.update_src_file(frame.file)
+		if hasattr(frame, 'line'):
+			self.src_line = int(frame.line)
+			self.log.debug("CURRENT LINE : %d" % self.src_line)
+		else:
+			self.src_line = None
 
 class CommandInput(Controller):
 	def __init__(self, gdbtui, win):
@@ -131,7 +153,7 @@ class TopLevelKeyboardInput(Controller):
 class CommandHandler(object):
 	def __init__(self, gdb):
 		self.gdb = gdb
-		self.commandQueue = EventQueue()
+		self.commandQueue = EventQueue(vsync = 1e-3)
 		self.onRun = self.commandQueue.schedule_handler(self._onRun)
 		self.onContinue = self.commandQueue.schedule_handler(self._onContinue)
 		self.onNext = self.commandQueue.schedule_handler(self._onNext)
@@ -152,11 +174,12 @@ class CommandHandler(object):
 	def _onQuit(self):
 		self.commandQueue.process = False
 
-class PyGdbTui(object):
+class PyGdbTui(TopLevelView):
 
 	def __init__(self, gdb, topwin):
 		self.gdb = gdb
 		self.sess = pygdb.GdbSession(gdb)
+		self.log = logging.getLogger('gdb')
 		self.topwin = topwin
 		curses.raw()
 		self.topwin.keypad(1)
@@ -166,12 +189,24 @@ class PyGdbTui(object):
 		
 		# Events
 		self.onStartCommandInput = EventSlot()
+		
+		# Event Handlers
+		self.sess.onProcessedResponse.subscribe(self.onGdbProcessedResponse)
+		self.scheduled_onGdbProcessedResponse = self.commandHandler.commandQueue.schedule_handler(self._onGdbProcessedResponse)
 
 		# Views
 		self.src_view = SourceFileView(self, self.topwin)
 
 		# Input
 		self.kb_input = TopLevelKeyboardInput(self, self.topwin)
+
+	def onGdbProcessedResponse(self, sess):
+		self.scheduled_onGdbProcessedResponse()
+	def _onGdbProcessedResponse(self):
+		self.log.debug("### REFRESH ###")
+		self.src_view.draw()
+		self.topwin.refresh()
+		self.src_view.client_area.refresh()
 
 if __name__ == '__main__':
 	
