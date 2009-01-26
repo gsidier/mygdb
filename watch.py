0054af3fc145dc3bdf5d263262104c8784058c32
gatino <greg@vaio.(none)>
2009-01-26 09:15:07 +0000

    Moved var path syntax to separate module.

--- watch.py
+++ watch.py
@@ -1,108 +1,53 @@
 import pygdb
-from recparse import *
 
-class PathParser(object):
-	"""
-	The Var Path Syntax described below describes var path parsers.
+class WatchedVar(object):
+	def __init__(self, gdbsess, var):
+		self.gdbsess = gdbsess
+		self.var = var
 	
-	A var path is of the form :
-		(ID | NUM) '.' (ID | NUM) '.' (ID | NUM) ...
-	
-	This is first "tokenized" by splitting the input string.
+	def _getchild(self, chld):
+		if self.var.children is None:
+			self.gdbsess.var_list_children(var.name)
+			return None
+		elif chld in self.var.children:
+			return self.var.children[
 
-	Then the token stream is passed to the parser, which returns a result having:
-	
-		* result.list : the list of matched tokens
-		* result.dict : the dict of name -> value named tokens matched
-	
-	as specified by the VarPathSyntax (see below).
-	
-	"""	
-	class Result(object):
-		pass
-	
-class VarPathSyntax(object):
+	def __getattr__(self, attr):
+		return self._getchild(attr)
 
-	"""
-	var path syntax:
-
-
-		ID = [a-zA-Z_0-9]+
-		VAR = '%' [a-zA-Z]
-
-		multspec
-		=	'?'
-		|	'*'
-		|	'+'
-		|	'{' num? ','? num? '}'
-
-		atom 
-		= 	ID
-		|	VAR
-		|	'(' path ')' multspec?
-
-		named_atom
-		=	atom ('[' ID ']')?
-
-		path
-		=	named_atom ('.' path)?
-	"""
-
-	lex = Lexer(
-		ID        = ( ALPHA | Literal('_') ) + ( ALPHANUM | Literal('_') ) * (0,) >= flatten_chars,
-		NUM       = DIGIT * (1,)         >= flatten_chars, 
-		VAR       = Literal('%') + ALPHA >= (lambda toks, val: val[1]),
-		OPT       = Literal('?'),
-		STAR      = Literal('*'),
-		PLUS      = Literal('+'),
-		LCURLY    = Literal('{'),
-		RCURLY    = Literal('}'),
-		COMMA     = Literal(','),
-		LPAREN    = Literal('('),
-		RPAREN    = Literal(')'),
-		DOT       = Literal('.'),
-		LSQUARE   = Literal('['),
-		RSQUARE   = Literal(']')
-	)
-
-
-	multspec = (
-			lex.OPT    .set_result (lambda toks, val: (0,1))
-		|	lex.STAR   .set_result (lambda toks, val: (0,))
-		|	lex.PLUS   .set_result (lambda toks, val: (1,))
-		|	(lex.LCURLY + Optional(lex.NUM) + Optional(lex.COMMA) + Optional(lex.NUM) + lex.RCURLY) .set_result (
-				lambda toks, val: (None if val[1] is None else int(val[1]), None if val[3] is None else int(val[3])))
-	)
-
-	path = Forward()
-
-	atom = (
-			lex.ID
-		|	lex.NUM
-		|	lex.VAR
-		|	lex.LPAREN + path + lex.RPAREN + Optional(multspec)
-	)
-
-	named_atom = atom + Optional( lex.LSQUARE + lex.ID + lex.RSQUARE >= (lambda toks, val: val[1]))
-
-	path << DelimitedList(named_atom, lex.DOT)
+	def __getitem__(self, idx):
+		return self._getchild(str(idx))
 
 class FilteredWatch(pygdb.VarWatcher):
 	def __init__(self, gdbsess, var):
 		VarWatcher.__init__(self, gdbsess, var, 'default')
+		self.children = {}
+		self.pyval = None
+		self.w = WatchedVar(gdbsess, var)
+
+	def _pyval(self):
+		pass
 	
-if __name__ == '__main__':
-	inputstr = 'foo.123.(zob.baz.%d[index])+'
-	chars = TokenStream(iter(inputstr))
+	def _children(self):
+		pass
+	
+	def onUpdate(self, var, upd):
+		self.children = self._children()
+		self.pyval = self._pyval()
 
-	tokens = VarPathSyntax.lex.lex(chars)
-	toks = list(tokens.unconsumed)
-	print toks
-	tokstream = TokenStream(iter(toks))
 
-	success, result = VarPathSyntax.path.try_parse(tokstream)
-	print success
-	if success:
-		print result.tokens
-		print result.value
 
+"""
+Common interface for watchable variables:
+
+var v,
+
+	v.name : var name eg "var1.foo.bar"
+	v.expr : var expr eg "bar"
+	v.type = var type eg "int", "struct {...}*", ...
+	v.value = string 
+	v.numchild = int
+	v.in_scope = in_scope
+	v.children = None
+
+"""
