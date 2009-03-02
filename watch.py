75244bcfdfd9c416a4087f189f7e30f1b1f93e71
gatino <greg@vaio.(none)>
2009-03-02 08:16:17 +0000

    Stop recomputing children of a watch if already being done.

--- watch.py
+++ watch.py
@@ -1,4 +1,5 @@
 import pygdb
+
 """
 Common interface for watchable variables:
 
@@ -23,18 +24,23 @@ class AbstractVar(object):
 	numchild = property(lambda self: self._numchild())
 	in_scope = property(lambda self: self._in_scope())
 	path_expr = property(lambda self: self._path_expr())
-	
+	_children_computing = False
 
 	def __init__(self, gdbsess, var):
 		self.gdbsess = gdbsess
 		self.var = var
 	
 	def _children(self):
+		if self._children_computing:
+			return {}
+		
 		if self.var.numchild == 0:
 			return {}
 		
 		if self.var.children is None:
+			self._children_computing = True
 			self.gdbsess.var_list_children(self.var.name, sync = True)
+			self._children_computing = False
 		
 		if self.var.children is None: # still
 			raise Exception, "Var get children failed."
