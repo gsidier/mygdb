cb5900ac9aaa7051e9508a3918bdd9de9c09954c
gatino <na@na.com>
2009-05-11 11:26:42 +0100

    Fix std::map casting. Cast char, const char, const int.

--- pywatch.py
+++ pywatch.py
@@ -8,7 +8,11 @@ class PyWatch(AbstractVar):
 	@classmethod
 	def _wrap(cls, sess, var):
 		if var.type is not None:
-			if var.type.startswith('std::basic_string<') and var.type[-1] == '>':
+			if var.type == 'char' or var.type == 'const char':
+				return CharWatch(sess, var)
+			elif var.type == 'int' or var.type == 'const int':
+				return IntWatch(sess, var)
+			elif var.type.startswith('std::basic_string<') and var.type[-1] == '>':
 				return StdStringWatch(sess, var)
 			elif var.type.startswith('std::pair<') and var.type[-1] == '>':
 				return StdPairWatch(sess, var)
@@ -18,8 +22,6 @@ class PyWatch(AbstractVar):
 				return StdMapWatch(sess, var)
 			elif var.type == 'char *':
 				return CharPtrWatch(sess, var)
-			elif var.type == 'int':
-				return IntWatch(sess, var)
 			elif var.type[-1] == '*':
 				return PtrWatch(sess, var)
 
@@ -30,7 +32,33 @@ class PyWatch(AbstractVar):
 	
 	def eval(self, expr, subs = ()):
 		var = self.register_watch(expr, subs)
-		return var.value
+		return var.pyval
+
+class CppChar(object):
+	
+	char = property(lambda self: chr(self.num))
+
+	def __init__(self, num):
+		self.num = num % 256
+	def __str__(self):
+		return self.char
+	def __repr__(self):
+		return "'%s' (%d)" % (self.char, self.num)
+
+class CharWatch(PyWatch):
+	def __init__(self, gdbsess, var):
+		PyWatch.__init__(self, gdbsess, var)
+	def _pyval(self):
+		return CppChar(int(self.value.split(" ")[0]))
+
+class CppPtr(object):
+	def __init__(self, addr, type):
+		self.addr = addr
+		self.type = type
+	def __str__(self):
+		return "(%s) %s" % (self.type, hex(self.addr))
+	def __repr__(self):
+		return "CPointer(%s, %d)" % (repr(self.type), repr(addr))
 
 class PtrWatch(PyWatch):
 	def __init__(self, gdbsess, var):
@@ -39,16 +67,7 @@ class PtrWatch(PyWatch):
 		s = self.value
 		addr = int(s[s.rfind('x')+1:], 16)
 		type = s[s.find('(')+1:s.rfind(')')]
-		return CPtr(addr, type)
-
-class CPtr(object):
-	def __init__(self, addr, type):
-		self.addr = addr
-		self.type = type
-	def __str__(self):
-		return "(%s) %s" % (self.type, hex(self.addr))
-	def __repr__(self):
-		return "CPointer(%s, %d)" % (repr(self.type), repr(addr))
+		return CppPtr(addr, type)
 
 class IntWatch(PyWatch):
 	def __init__(self, gdbsess, var):
@@ -124,29 +143,28 @@ class CharPtrWatch(PyWatch):
 class StdMapWatch(PyWatch):
 	def __init__(self, gdbsess, var):
 		PyWatch.__init__(self, gdbsess, var)
-		self.root = self.register_watch(
-			"(%s)._M_t._M_impl._M_header",
+		self.ptr_root = self.register_watch(
+			"(%s)._M_t._M_impl._M_header._M_parent",
 			(self.var,))
 		self.cpptype = parse_cpptype(self.type).value
 	
 	def _pyval(self):
 		res = {}
-		ptr_root = self.register_watch("&(%s)", (self.root,))
-		
+	
+		pair_type = "std::pair< const %s, %s >" % (
+			self.cpptype.local.template_args[0],
+			self.cpptype.local.template_args[1])
+	
 		def rec(curr):
-			ptr_p = self.register_watch(
-				"(%s)._M_parent",
+			if curr.pyval.addr == 0:
+				return
+			
+			casted = self.register_watch(
+				"('std::_Rb_tree_node< " + pair_type + " >' *)(%s)",
 				(curr,))
-			if ptr_p.pyval.addr != 0: # data node
-				pair_type = "std::pair< const %s, %s >" % (
-						self.cpptype.local.template_args[0],
-						self.cpptype.local.template_args[1])
-				casted = self.register_watch(
-					"('std::_Rb_tree_node< " + pair_type + " >' *)(%s)",
-					(ptr_p,))
-				key = self.eval("(%s)._M_value_field.first", (casted,))
-				value = self.eval("(%s)._M_value_field.second", (casted,))
-				res[key] = value
+			key = self.eval("(%s)._M_value_field.first", (casted,))
+			value = self.eval("(%s)._M_value_field.second", (casted,))
+			res[key] = value
 			
 			ptr_r = self.register_watch(
 				"(%s)._M_right",
@@ -154,10 +172,9 @@ class StdMapWatch(PyWatch):
 			ptr_l = self.register_watch(
 				"(%s)._M_left", 
 				(curr,))
-			if ptr_r.pyval.addr != curr.pyval.addr:
-				rec(ptr_r)
-			if ptr_l.pyval.addr != curr.pyval.addr:
-				rec(ptr_l)
+			rec(ptr_r)
+			rec(ptr_l)
 			
-		rec(ptr_root)
+		rec(self.ptr_root)
 		return res
+
